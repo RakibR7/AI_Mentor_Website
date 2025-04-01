4106659996d2edd6b369bd3d4840f381daededf4
Rakib Rana <G00408644@atu.ie>
2025-04-01 10:52:20 +0100

    v11.3 added credits at bottom right with styles via position: fixed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,24 @@
-// src/pages/Home.js
 import React from "react"
-import { useNavigate } from "react-router-dom"
 import "./Home.css"
 
 function Home() {
-  const navigate = useNavigate()
-
-  const handleExploreTutors = () => {
-    navigate("/subject-tutor")
-  }
-
   return (
     <div className="HomePage">
       <h1>Mentor AI</h1>
       <h2>Empowering Your Learning Journey</h2>
-      <p>Experience interactive tutoring in Biology, Math, and English through our AI-driven platform.</p>
-      <p>Get instant feedback, explanations, and guidance — all tailored to your needs.</p>
-      <button onClick={handleExploreTutors}>Explore Tutors</button>
+      <p>Experience interactive tutoring through our AI-driven platform.</p>
+      <button>Explore Tutors</button>
+
+      <div className="credit-homepage">
+        Ocean - Photo by Engin Akyurt from{" "}
+        <a
+          href="https://www.pexels.com/photo/close-up-photo-of-blue-body-of-water-1435752/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Pexels
+        </a>
+      </div>
     </div>
   )
 }
